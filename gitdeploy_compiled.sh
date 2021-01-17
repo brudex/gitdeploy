ae9c19a1851afdc3cfa16478055ef797298253a6
NanaSafo <brudexgh@gmail.com>
2021-01-17 17:39:09 +0000

    compiled project deploy script

--- gitdeploy_compiled.sh
+++ gitdeploy_compiled.sh
@@ -3,13 +3,14 @@ echo -n "Enter app name :"
 read appName
 echo -n "Enter git folder? /var/repo/${appName}.git: "
 read repoName
-echo -n "Deploy script file. Located in project root- Default 'run.sh' : "
+echo -n "Deploy script file. Located in project root ? run.sh : "
 read scriptFile
 
-if [ -z $repoName]; then
+if [ -z $repoName ]; then
   repoName="/var/repo/${appName}.git"
 fi
-if [ ${repoName: -4} -ne ".git" ]; then
+if [[ ${#repoName} > 4  && ${repoName: -4} != ".git" ]]; then
+  echo "Adding .git to end of file name"
   repoName="${repoName}.git"
 fi
 #create repo dir if not exist
@@ -17,7 +18,7 @@ if [[ ! -d $repoName ]]; then
   mkdir -p $repoName
 fi
 cd $repoName && mkdir "${appName}_SRC_TREE"
-if [ -z $scriptFile];
+if [ -z $scriptFile ]; then
   scriptFile="run.sh"
 fi
 srcDir="${repoName}/${appName}_SRC_TREE"
@@ -28,6 +29,6 @@ git --work-tree=${srcDir} --git-dir=${repoName} checkout -f
 sleep 5
 chmod +x ${srcDir}/${scriptFile}
 export SRCDIR=${srcDir}
-. {srcDir}/${scriptFile}
+. ${srcDir}/${scriptFile} ${srcDir}
 " >> post-receive
 chmod +x post-receive
